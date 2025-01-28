9746b392a90b6dd3d6481400e054e91384fcfc42
Abdalla-Elhagar <nanoelhgar@gmail.com>
2025-01-28 23:39:12 +0200

    1

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,21 @@ export default defineConfig({
   optimizeDeps: {
     include: ['swiper'],
   },
+  build: {
+    chunkSizeWarningLimit: 5000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
+
